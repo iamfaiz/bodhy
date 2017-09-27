9f97bce54ea9d067817e4e8e9e3b0de3abc2adc5
Rana Faiz Ahmad <ranafaizahmad@localhost.localdomain>
2017-09-27 23:43:34 +0500

    Load all the css and js from single files.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var mustache = require('gulp-mustache');
 var htmlData = require('./src/data');
+var browserify = require('gulp-browserify');
 
 gulp.task('html', function () {
 	return gulp.src('./src/templates/index.html')
@@ -15,7 +16,17 @@ gulp.task('sass', function () {
 		.pipe(gulp.dest('./dist/css'));
 });
 
+gulp.task('scripts', function () {
+	return gulp.src('./src/js/main.js')
+		.pipe(browserify({
+			insertGlobals : true,
+			debug : !process.env.production
+		  }))
+		.pipe(gulp.dest('./dist/js'));
+});
+
 gulp.task('watch', function () {
 	gulp.watch('./src/templates/**/*.html', ['html']);
 	gulp.watch('./src/sass/**/*.scss', ['sass']);
+	gulp.watch('./src/js/**/*.js', ['scripts']);
 });
---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+// Load jQuery on the page.
+window.jQuery = $ = require('jquery');
+
+// Load popper.js (a dependency of bootstrap 4).
+window.Popper = require('popper.js');
+
+// Load bootstrap's blank plugin file.
+require('bootstrap');
+
+// Load bootstrap's modal plugin.
+require('bootstrap/js/dist/modal.js');
+
+// Enable the smooth scroll of # based internal links.
+require('./smooth-scroll');
+
+// Make the navigation bar responsive.
+require('./responsive-navigation');
+
+// A helper function to control YouTube iframe.
+window.callPlayer = require('./call-player');
+
+// Handle the play and pause of the video on the page based on the modal's status.
+require('./youtube-video');
